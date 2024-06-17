282569cbc1c4102e327916026a3f37c5dfbb6c25
Bob6541684 <101238821+Bob6541684@users.noreply.github.com>
2024-06-17 11:06:56 +0800

    Update main.cpp

--- 數獨/main.cpp
+++ 數獨/main.cpp
@@ -10,17 +10,22 @@ int main() {
     FillBlank fillblank; //產生填空物件
     Answer answer; //產生答案物件
 
-   blank.receive_board(NewBoard.get_board()); // 將棋盤物件產生的棋盤複製到挖空物件裡
-  
+    blank.receive_board(NewBoard.get_board()); // 將棋盤物件產生的棋盤複製到挖空物件裡
+    answer.receive_original_board(NewBoard.get_board()); // 將棋盤物件產生的棋盤複製到答案物件裡
 
     int n, x, y, number;
-  cout << "請輸入想要挖多少空格(1~81)？";//
+    cout << "請輸入想要挖多少空格(1~81)？";
     cin >> n; // 輸入數字要挖多少空格
     while (n < 1 || n > 81) {
         cout << "請重新輸入...(1~81)";
         cin >> n;
     }
 
+    blank.hole_board(n); // 將上面輸入的數字傳到挖空物件裡，並進行挖空
+    fillblank.receive_board(blank.return_board()); // 將挖好空格的棋盤傳到填空物件裡
+    answer.receive_revise_board(fillblank.return_board()); // 將未完成填空的棋盤傳到答案進行是否正確
+    fillblank.get_board(); // 列印未完成填空的棋盤
+    
     while (answer.comparison()) { // 判斷有有完成挖空
         cin >> x >> y >> number;
         fillblank.setX(x);
