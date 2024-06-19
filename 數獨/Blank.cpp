268b1b93f78932625705d5f9c921b1b21d4bb3b9
cjs20040817 <126649275+cjs20040817@users.noreply.github.com>
2024-06-19 11:29:24 +0800

    Update Blank.cpp

--- 數獨/Blank.cpp
+++ 數獨/Blank.cpp
@@ -7,7 +7,7 @@ void Blank::receive_board(vector <int > x) {
 
 void Blank::hole_board(int x) {
 
-    Hollow_quantity = x;
+    Hollow_quantity = x;// 設定挖掘的空洞數量為 x
     int no_n = 0;
     for (int i = 0; i < Hollow_quantity; i++) {
         revise_board[rand() % 81] = 0;//隨機取空格
@@ -15,14 +15,14 @@ void Blank::hole_board(int x) {
     while (Hollow_quantity != no_n) {//確定空格有沒有到要挖的數量
         no_n = 0;
         for (int i = 0; i < 81; i++) {
-            if (revise_board[i] == 0) no_n++;//數空格
+            if (revise_board[i] == 0) no_n++;// 計算目前已挖掘的空洞數量
         }
         for (int i = 0; i < Hollow_quantity - no_n; i++) {
-            revise_board[rand() % 81] = 0;
+            revise_board[rand() % 81] = 0; // 再次隨機挖掘一個格子，直到達到預定的空洞數量
         }
     }
 }
-
+// 回傳遊戲板的當前狀態，以整數向量的形式返回
 vector<int> Blank::return_board() {
     return revise_board;
 }
